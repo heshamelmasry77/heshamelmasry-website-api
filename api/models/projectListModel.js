4ed6cf55d45f22b804b39901a9e375271f078df5
Hesham El Masry <heshamelmasry@outlook.com>
2017-11-17 16:01:26 +0200

    adding desc and url

--- api/models/projectListModel.js
+++ api/models/projectListModel.js
@@ -2,32 +2,35 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var ProjectSchema = new Schema({
   name: {
     type: String,
-    required: 'Kindly enter the name of the project'
+    required: 'Kindly enter the name of the project',
   },
   description: {
     type: String,
-    required: 'Kindly enter the description of the project'
-  }
-  ,picture: {
+    required: 'Kindly enter the description of the project',
+  },
+  about: {
     type: String,
-    required: 'Kindly enter the picture of the project'
+    required: 'Kindly enter the about text of the project',
+  }, picture: {
+    type: String,
+    required: 'Kindly enter the picture of the project',
   },
   Created_date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   status: {
-    type: [{
-      type: String,
-      enum: ['pending', 'ongoing', 'completed']
-    }],
-    default: ['pending']
-  }
-
+    type: [
+      {
+        type: String,
+        enum: ['pending', 'ongoing', 'completed'],
+      }],
+    default: ['pending'],
+  },
+  
 });
 
 module.exports = mongoose.model('Projects', ProjectSchema);
